074ce334cadfd32b1bc4c0361908a2b74a194297
Jose Riquelme <joseriquelmeleon@gmail.com>
2021-02-16 16:45:19 -0300

    Docs agregados

---
+++ docs/precache-manifest.20fb29dc654361b10d5a382037165f88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93c6d64f384ccfad3cc3fb2516d8b77a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3aabd369d991b28cff1e",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aabd369d991b28cff1e",
+    "url": "/static/js/main.78887f3b.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
